eeae9e151677f5d206a06d38a7ac3c341003a8dd
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-02-14 23:59:10 +1100

    create basic Decoder class
    stores key (answer)
    and codes: 12 row of guess code

---
+++ lib/decoder.rb
@@ -0,0 +1,11 @@
+class Decoder
+  def initialize(key)
+    @key = key
+    @codes = [
+      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil],
+      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil],
+      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil],
+      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil]
+    ]
+  end
+end
