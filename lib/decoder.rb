84f29b9d29a890f18ea9ed648a55a657bc574410
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-02-15 22:42:06 +1100

    update Decoder class

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -1,11 +1,34 @@
 class Decoder
+  attr_reader :codes, :signals
+
+  # @param [Array<Integer>] key
   def initialize(key)
     @key = key
-    @codes = [
-      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil],
-      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil],
-      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil],
-      [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil]
-    ]
+    @codes = Array.new(12) { [nil, nil, nil, nil] }
+    @signals = Array.new(12) { [nil, nil, nil, nil] }
+    @turn = 0
+  end
+
+  def decode(code)
+    @codes[@turn] = code
+    create_signal(code)
+    @turn += 1
+    code == @key
+  end
+
+  private
+
+  # @param [Array<Integer>] code
+  def create_signal(code)
+    signal_half = 0
+    signal_full = 1
+
+    4.times do |i|
+      if code[i] == @key[i]
+        @signals[@turn][i] = signal_full
+      elsif @key.include? code[i]
+        @signals[@turn][i] = signal_half
+      end
+    end
   end
 end
