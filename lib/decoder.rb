cf1bd50e55049bb421ef670ab5f6aa7a3da04949
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-02-15 23:49:21 +1100

    Decoder: default ' ', signal with 0/1/2 number
    default @codes and @signals array to use ' '
    for rendering purposes.
    Using nil will result in no space, messing up the display
    
    use 0/1/2 as signals: have signal for all 3 state
    empty/half/full
    also makes it easier to shuffle

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -4,8 +4,8 @@ class Decoder
   # @param [Array<Integer>] key
   def initialize(key)
     @key = key
-    @codes = Array.new(12) { [nil, nil, nil, nil] }
-    @signals = Array.new(12) { [nil, nil, nil, nil] }
+    @codes = Array.new(12) { [' ', ' ', ' ', ' '] }
+    @signals = Array.new(12) { [' ', ' ', ' ', ' '] }
     @turn = 0
   end
 
@@ -20,15 +20,16 @@ class Decoder
 
   # @param [Array<Integer>] code
   def create_signal(code)
-    signal_half = 0
-    signal_full = 1
-
+    signal_arr = []
     4.times do |i|
-      if code[i] == @key[i]
-        @signals[@turn][i] = signal_full
-      elsif @key.include? code[i]
-        @signals[@turn][i] = signal_half
-      end
+      signal_arr << if code[i] == @key[i]
+                      2 # full
+                    elsif @key.include? code[i]
+                      1 # half
+                    else
+                      0 # empty
+                    end
     end
+    @signals[@turn] = signal_arr.shuffle
   end
 end
